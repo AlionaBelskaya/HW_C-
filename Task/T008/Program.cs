4b474b1a1dd82efd2f5e84ed5e9167484b98cceb
AlionaBelskaya <6066188@gmail.com>
2022-12-02 10:41:51 +0300

    Added T008

---
+++ Task/T008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+int firstNumber = 2; // Первое четное число после 1
+int secondNumber = Convert.ToInt32(Console.ReadLine()); // число N
+while (firstNumber <= secondNumber)
+{  
+   Console.WriteLine(firstNumber);
+   firstNumber = firstNumber + 2;
+}
